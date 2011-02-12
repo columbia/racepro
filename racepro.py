d1a895159ad91544e6e09041988dee8b75b16d11
Oren Laadan <orenl@cs.columbia.edu>
2011-02-11 19:10:11 -0500

    demonstrate fork/exit/exit graph analysis

--- racepro.py
+++ racepro.py
@@ -1,4 +1,5 @@
 import sys
+import pygraphviz as pgv
 import mmap
 import scribe
 
@@ -16,6 +17,19 @@ class Resource:
         self.events = list()
 
 class Session:
+
+    NR_fork = 2
+    NR_clone = 120
+    NR_vfork = 190
+    NR_exit = 1
+    NR_exit_group = 252
+    NR_waitpid = 7
+    NR_wait4 = 114
+    NR_waitid = 284
+
+    __sys_fork = set([NR_fork, NR_clone, NR_vfork])
+    __sys_exit = set([NR_exit, NR_exit_group])
+    __sys_wait = set([NR_waitpid, NR_wait4, NR_waitid])
 
     def __init__(self, logfile):
         self.process_map = dict()
@@ -83,3 +97,41 @@ class Session:
                 ev = self.event_list[r[2]]
                 self.event_list[r[2]] = (ev[0:3], resource, i)
 
+    def node_name(self, pid, syscall, arg):
+        return str(pid) + ': ' + syscall + '(' + str(arg) + ')'
+
+    def _process_graph(self, graph, proc):
+        pid = proc.pid
+        ancestor = str(pid)
+        for info, event, i in proc.events:
+            if not isinstance(event, scribe.EventSyscallExtra):
+                continue
+            if event.nr in Session.__sys_fork and event.ret > 0:
+                newpid = event.ret
+                parent = self.node_name(pid, 'fork', newpid)
+                child = str(newpid)
+                graph.add_node(parent, fontsize='8', width='0.2', height='0.2')
+                graph.add_edge((ancestor, parent))
+                graph.add_node(child, color='blue', style='filled')
+                graph.add_edge((parent, child), label='fork')
+                graph = self._process_graph(graph, self.process_map[newpid])
+                ancestor = parent
+            elif event.nr in Session.__sys_wait and event.ret < sys.maxint:
+                newpid = event.ret
+                node = self.node_name(pid, 'wait', newpid)
+                child = self.node_name(newpid, 'exit', '')
+                graph.add_node(node, fontsize='8', width='0.2', height='0.2')
+                graph.add_edge((ancestor, node))
+                graph.add_edge((child, node), label='wait')
+                ancestor = node
+            elif event.nr in Session.__sys_exit:
+                node = self.node_name(pid, 'exit', '')
+                graph.add_node(node, fontsize='8', width='0.2', height='0.2')
+                graph.add_edge((ancestor, node))
+        return graph
+
+    def process_graph(self):
+        graph = pgv.AGraph(directed=True)
+        graph.add_node('1', fillcolor='blue', style='filled')
+        graph = self._process_graph(graph, self.process_list[0])
+        return graph
