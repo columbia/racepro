15765fd0c02d36b5a942deea5256374504ad93ef
Oren Laadan <orenl@cs.columbia.edu>
2011-02-24 14:56:02 -0500

    graph: add dependencies through resources
    the graph now has three modes:
    1) only processes flow (only fork, wait, exit)
    2) execution flow with all system calls
    3) all system calls and resource dependencies
    
    while at it, also modified the code so that events as seen by
    processes, resources and sessions, are objects of their own rather
    than cryptic tuples.
    
    the graph code is now based on python-networkx module instead of
    tpygraphviz from before.

--- racepro.py
+++ racepro.py
@@ -4,39 +4,61 @@ import scribe
 import unistd
 import networkx
 
+class ProcessEvent:
+    """An event from the point of view of a process:
+    @info: pointer to scribe's info
+    @event: pointer to scribe's event
+    @index: index of event in global log
+    @syscnt: syscall count (per process)
+    """
+    __slots__ = ('info', 'event', 'index', 'syscnt')
+
+    def __init__(self, info, event, index, syscnt):
+        self.info = info
+        self.event = event
+        self.index = index
+        self.syscnt = syscnt
+
 class Process:
     """Describe execution log of a single process.
-
     @pid: pid of the process
     @name: name of the program
     @events: (ordered) list of events performed by this process
-
-    @sys, @reg: track current syscall/regs events (temporary)
-
-    Events are: (info, event, i), where @info and @events are scribe's
-    event description, and @i is the index of the event in the global
-    events list.
+    @sys: track current (last) syscall event (temporary)
+    @reg: track current (last) regs events (temporary)
     """
+    __slots__ = ('pid', 'name', 'events', 'count', 'sysind', 'regind')
 
     def __init__(self, pid):
         self.pid = pid
-        self.name = '-'
+        self.name = None
         self.events = list()
-        self.sys = -1
-        self.reg = -1
+        self.syscnt = 0
+        self.sysind = -1
+        self.regind = -1
         
+class ResourceEvent:
+    """An event from the point of view of a resource:
+    @info: pointer to scribe's info
+    @event: pointer to scribe's event
+    @index: index of event in global log
+    @sysind: index of owning syscall in global log
+    """
+    __slots__ = ('info', 'event', 'index', 'sysind')
+
+    def __init__(self, info, event, index, sysind):
+        self.info = info
+        self.event = event
+        self.index = index
+        self.sysind = sysind
+
 class Resource:
     """Describe execution log related to a resource isntance.
-
     @type: type of resource
     @id: unique identifier of the resource
     @events: (ordered) list of events affecting this resource
-
-    Events are: (info, event, i, sys), where @info and @events are
-    scribe's event description, @i is the index of the event in the
-    global events list, and @sys is the index of the system call
-    containing this event.
     """
+    __slots__ = ('type', 'id', 'events')
 
     def __init__(self, event):
         self.type = event.type
@@ -50,6 +72,35 @@ class Action:
         self.action = action
         self.arg1 = arg1
 
+class SessionEvent:
+    """An event from the point of view of a session:
+    @info: pointer to scribe's info
+    @event: pointer to scribe's event
+    @proc: pointer to respective Process
+    @pindex: index of event inside respective Process
+    @resource: pointer to respective Resource
+    @rindex: index of event inside respective Resource
+    @sysind: index of owning syscall event
+    @regind: index of owning regs event
+    """
+    __slots__ = ('info', 'event',
+                 'proc', 'pindex',
+                 'resource', 'rindex',
+                 'sysind', 'regind')
+
+    def __init__(self, info, event,
+                 proc, pindex,
+                 resource, rindex,
+                 sysind, regind):
+        self.info = info
+        self.event = event
+        self.proc = proc
+        self.pindex = pindex
+        self.resource = resource
+        self.rindex = rindex
+        self.sysind = sysind
+        self.regind = regind
+
 class Session:
     """Describe exceution log of an entire session.
 
@@ -62,12 +113,6 @@ class Session:
     @resource_map: map a unique identifier to the coresponding Resource
     @process_list: list of all Resource instances
     @events_list: list of all events (pointers to Process/Resources)
-
-    Event are: (info, event, proc, plen, resource, rlen, sys, reg),
-    where: @info and @event are scribe's event description, @proc is
-    the Process instance and @plen the index in its events list, and
-    @resource is the resource index and @rlen the index in its events
-    list.
     """
 
     # order of arguments: ebx, ecx, edx, esi ...
@@ -75,29 +120,29 @@ class Session:
     def parse_syscall(self, i, j):
         """Parse a syscall event"""
 
-        syscall = self.events_list[i][1]
-        args = self.events_list[j][1].args
-        data = self.events_list[i+1][1]
+        e_syscall = self.events_list[i].event
+        e_data = self.events_list[i+1].event
+        args = self.events_list[j].event.args
         ret = unistd.syscall_ret(syscall.ret)
 
-        if syscall.nr == unistd.Syscalls.NR_open:
+        if e_syscall.nr == unistd.Syscalls.NR_open:
             print('[%d] open("%s", %#x, %#3o) = %ld' %
-                  (i, data.data, args[1], args[2], ret))
-        if syscall.nr == unistd.Syscalls.NR_close:
+                  (i, e_data.data, args[1], args[2], ret))
+        if e_syscall.nr == unistd.Syscalls.NR_close:
             print('[%d] close(%d) = %ld' %
                   (i, args[0], ret))
-        if syscall.nr == unistd.Syscalls.NR_access:
+        if e_syscall.nr == unistd.Syscalls.NR_access:
             print('[%d] access("%s", %#3o) = %ld' %
-                  (i, data.data, args[0], ret))
-        if syscall.nr == unistd.Syscalls.NR_execve:
+                  (i, e_data.data, args[0], ret))
+        if e_syscall.nr == unistd.Syscalls.NR_execve:
             print('[%d] execve("%s", %#x, %#x) = %ld' %
-                  (i, data.data, args[0], args[1], ret))
-        if syscall.nr == unistd.Syscalls.NR_stat:
+                  (i, e_data.data, args[0], args[1], ret))
+        if e_syscall.nr == unistd.Syscalls.NR_stat:
             print('[%d] stat("%s", %#x) = %ld' %
-                  (i, data.data, args[0], ret))
-        if syscall.nr == unistd.Syscalls.NR_stat64:
+                  (i, e_data.data, args[0], ret))
+        if e_syscall.nr == unistd.Syscalls.NR_stat64:
             print('[%d] stat64("%s", %#x, %#x) = %ld' %
-                  (i, data.data, args[0], args[1], ret))
+                  (i, e_data.data, args[0], args[1], ret))
 
     def print_process(self, pid):
         """Print all the events of a process"""
@@ -107,10 +152,10 @@ class Session:
             print('No such process with pid %d' % (pid))
             return
 
-        for (info, event, i) in proc.events:
-            if isinstance(event, scribe.EventSyscallExtra):
+        for p_ev in proc.events:
+            if isinstance(p_ev.event, scribe.EventSyscallExtra):
                 ev = self.events_list[i]
-                self.parse_syscall(ev[6], ev[7])
+                self.parse_syscall(p_ev.sysind, p_ev.regind)
 
     def load_events(self, logfile):
         """Parse the scribe log from @logfile"""
@@ -123,14 +168,15 @@ class Session:
         # @i tracks current index in self.events
         proc = None
         pid = 0
-        i = -1
+        ind = -1
 
         # parse events
         for (info, event) in e:
-            i += 1
+            ind += 1
 
             if isinstance(event, scribe.EventPid):
-                self.events_list.append((info, event, None, 0, None, 0, 0, 0))
+                s_ev = SessionEvent(info, event, None, 0, None, 0, 0, 0)
+                self.events_list.append(s_ev)
                 pid = info.pid
                 try:
                     proc = self.process_map[pid]
@@ -141,16 +187,18 @@ class Session:
                 continue
 
             if pid == 0:
-                self.events_list.append((info, event, None, 0, None, 0, 0, 0))
+                s_ev = SessionEvent(info, event, None, 0, None, 0, 0, 0)
+                self.events_list.append(s_ev)
                 continue;
 
             if isinstance(event, scribe.EventRegs):
-                proc.reg = i
+                proc.regind = ind
             elif isinstance(event, scribe.EventSyscallExtra):
-                proc.sys = i
+                proc.syscnt += 1
+                proc.sysind = ind
             elif isinstance(event, scribe.EventSyscallEnd):
-                proc.reg = -1
-                proc.sys = -1
+                proc.regind = -1
+                proc.sysind = -1
 
             if isinstance(event, scribe.EventResourceLockExtra):
                 if event.id not in self.resource_map:
@@ -158,28 +206,28 @@ class Session:
                     self.resource_map[event.id] = resource
                     self.resource_list.append(resource)
                 resource = self.resource_map[event.id]
-                resource.events.append((info, event, i, sys))
+                r_ev = ResourceEvent(info, event, ind, proc.sysind)
+                resource.events.append(r_ev)
 
-            plen = len(proc.events)
-            proc.events.append((info, event, i))
+            s_ev = SessionEvent(info, event, proc, len(proc.events),
+                                None, 0, proc.sysind, proc.regind)
+            self.events_list.append(s_ev)
 
-            self.events_list.append((info, event,
-                                     proc, plen,
-                                     None, 0,
-                                     proc.sys, proc.reg))
+            p_ev = ProcessEvent(info, event, ind, proc.syscnt)
+            proc.events.append(p_ev)
 
             if isinstance(event, scribe.EventQueueEof):
                 proc = None
 
         # sort events per resource
         for resource in self.resource_list:
-            i = 0
-            resource.events.sort(key=lambda ev: ev[1].serial)
-            for r in resource.events:
-                i += 1
-                ev = self.events_list[r[2]]
-                # adjust pointers to event in Resource
-                self.events_list[r[2]] = ev[0:4] + (resource, i) + ev[6:8]
+            ind = 0
+            resource.events.sort(key=lambda s_ev: s_ev.event.serial)
+            for r_ev in resource.events:
+                s_ev = self.events_list[r_ev.index]
+                s_ev.resource = resource
+                s_ev.rindex = ind
+                ind += 1
 
     def save_events(self, logfile,
                     pid_bookmark = None,
@@ -212,7 +260,9 @@ class Session:
         if pid_cutoff is None:
             pid_cutoff = dict()
 
-        for (info, event) in (e[0:2] for e in self.events_list):
+        for s_ev in self.events_list:
+            info = s_ev.info
+            event = s_ev.event
             pid = info.pid
 
             # pid first time ?
@@ -297,10 +347,13 @@ class Session:
                 e = scribe.EventQueueEof()
                 logfile.write(e.encode())
 
+##############################################################################
+#   generate thread calls graph (fork,exit,wait,syscalls)
+
     def __make_node(self, pid, syscall):
         return str(pid) + ':' + str(syscall)
 
-    def __build_graph(self, graph, proc):
+    def __build_graph(self, graph, proc, full=False):
         """Build the execution DAG from the execution log, as follows:
         - The first node is '0'
         - New processes begin with a node 'pid'
@@ -312,41 +365,71 @@ class Session:
         - Resource access add edge from the previous user to current
         (note: attributes must be strings)
         """
-        syscall = 0
+        print(full)
         pid = proc.pid
         ancestor = str(pid)
-        for info, event, i in proc.events:
+        for p_ev in proc.events:
+            event = p_ev.event
             if not isinstance(event, scribe.EventSyscallExtra):
                 continue
-            syscall += 1
             if event.nr in unistd.Syscalls.SYS_fork and event.ret >= 0:
                 newpid = event.ret
-                parent = self.__make_node(pid, syscall)
-                graph.add_node(parent, index=str(i))
+                parent = self.__make_node(pid, p_ev.syscnt)
+                graph.add_node(parent, index=str(p_ev.index))
                 graph.add_edge(ancestor, parent)
                 child = str(newpid)
                 graph.add_node(child)
                 graph.add_edge(parent, child)
-                graph = self.__build_graph(graph, self.process_map[newpid])
+                self.__build_graph(graph, self.process_map[newpid], full)
                 ancestor = parent
             elif event.nr in unistd.Syscalls.SYS_wait and event.ret >= 0:
                 newpid = event.ret
-                node = self.__make_node(pid, syscall)
-                graph.add_node(node, index=str(i))
+                node = self.__make_node(pid, p_ev.syscnt)
+                graph.add_node(node, index=str(p_ev.index))
                 graph.add_edge(ancestor, node)
                 child = self.__make_node(newpid, 'exit')
                 graph.add_edge(child, node)
                 ancestor = node
             elif event.nr in unistd.Syscalls.SYS_exit:
                 node = self.__make_node(pid, 'exit')
-                graph.add_node(node, index=str(i))
+                graph.add_node(node, index=str(p_ev.index))
                 graph.add_edge(ancestor, node)
-        return graph
+                ancestor = node
+            elif full:
+                node = self.__make_node(pid, str(p_ev.syscnt))
+                graph.add_node(node, index=str(p_ev.index))
+                graph.add_edge(ancestor, node)
+                ancestor = node
 
-    def make_graph(self):
+    def __refine_graph(self, graph):
+        for resource in self.resource_list:
+            r_ev = resource.events[0]
+            p_proc = self.events_list[r_ev.index].proc
+            p_pind = self.events_list[r_ev.index].pindex
+
+            for r_ev in resource.events:
+                n_proc = self.events_list[r_ev.index].proc
+                n_pind = self.events_list[r_ev.index].pindex
+                if n_proc != p_proc:
+                    src = self.__make_node(p_proc.pid,
+                                           str(p_proc.events[p_pind].syscnt))
+                    dst = self.__make_node(n_proc.pid,
+                                           str(n_proc.events[n_pind].syscnt))
+                    graph.add_edge(src, dst)
+                    p_proc = n_proc
+                    p_pind = n_pind
+
+    def make_graph(self, full=False, refine=False):
         graph = networkx.DiGraph()
         graph.add_node('1')
-        self.__build_graph(graph, self.process_list[0])  # processes
+
+        # build initial graph
+        self.__build_graph(graph, self.process_list[0], full)
+
+        if refine:
+            # add dependencies of resources
+            self.__refine_graph(graph)  # resources
+
         return graph
 
     def __init__(self):
@@ -355,54 +438,6 @@ class Session:
         self.resource_map = dict()
         self.resource_list = list()
         self.events_list = list()
-
-
-##############################################################################
-# generate thread calls graph (fork,exit,wait)
-#
-
-import pygraphviz as pgv
-
-def node_name(pid, syscall, arg):
-    return str(pid) + ': ' + syscall + '(' + str(arg) + ')'
-
-def _process_graph(session, graph, proc):
-    pid = proc.pid
-    ancestor = str(pid)
-    for info, event, i in proc.events:
-        if not isinstance(event, scribe.EventSyscallExtra):
-            continue
-        ret = unistd.syscall_ret(event.ret)
-        if event.nr in unistd.Syscalls.SYS_fork and ret >= 0:
-            newpid = event.ret
-            parent = node_name(pid, 'fork', newpid)
-            child = str(newpid)
-            graph.add_node(parent, fontsize='8', width='0.2', height='0.2')
-            graph.add_edge((ancestor, parent))
-            graph.add_node(child, color='blue', style='filled')
-            graph.add_edge((parent, child), label='fork')
-            graph = _process_graph(session, graph, session.process_map[newpid])
-            ancestor = parent
-        elif event.nr in unistd.Syscalls.SYS_wait and ret >= 0:
-            newpid = event.ret
-            node = node_name(pid, 'wait', newpid)
-            child = node_name(newpid, 'exit', '')
-            graph.add_node(node, fontsize='8', width='0.2', height='0.2')
-            graph.add_edge((ancestor, node))
-            graph.add_edge((child, node), label='wait')
-            ancestor = node
-        elif event.nr in unistd.Syscalls.SYS_exit:
-            node = node_name(pid, 'exit', '')
-            graph.add_node(node, fontsize='8', width='0.2', height='0.2')
-            graph.add_edge((ancestor, node))
-    return graph
-
-def process_graph(session):
-    graph = pgv.AGraph(directed=True)
-    graph.add_node('1', fillcolor='blue', style='filled')
-    graph = _process_graph(session, graph, session.process_list[0])
-    return graph
-
 
 ##############################################################################
 # main
@@ -425,7 +460,7 @@ if __name__ == "__main__":
     parser.disable_interspersed_args()
     (options, cmd) = parser.parse_args()
 
-    if len(cmd) > 1 or cmd[0] not in ['graph', 'inject', 'test']:
+    if len(cmd) > 1 or cmd[0] not in ['graph', 'inject']:
         parser.error('Unknown command')
 
     if not options.logfile:
@@ -443,9 +478,12 @@ if __name__ == "__main__":
     s.load_events(f)
 
     if cmd[0] == 'graph':
-        g = process_graph(s)
-        g.layout(prog='dot')
-        g.draw(options.outfile)
+        g = s.make_graph(False, False)
+        print(networkx.convert.to_edgelist(g))
+        networkx.write_dot(g, options.outfile + '-proc.dot')
+        g = s.make_graph(True, True)
+        print(networkx.convert.to_edgelist(g))
+        networkx.write_dot(g, options.outfile + '-full.dot')
     elif cmd[0] == 'inject':
         a1 = Action(scribe.SCRIBE_INJECT_ACTION_SLEEP, 1000)
         a2 = Action(scribe.SCRIBE_INJECT_ACTION_SLEEP, 2000)
@@ -458,9 +496,5 @@ if __name__ == "__main__":
             print('Failed to open output file')
             exit(1)
         s.save_events(f, None, pid_cutoff, pid_inject)
-    elif cmd[0] == 'test':
-        g = s.make_graph()
-        print(networkx.convert.to_edgelist(g))
-        networkx.write_dot(g, options.outfile)
 
     exit(0)
