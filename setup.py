9851c2c7f7a06b8d3f6acc3dd974ceb187bc5b05
Oren Laadan <orenl@cs.columbia.edu>
2011-03-02 21:11:06 -0500

    prepare setup.py for auto-install

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name = 'RacePro',
+    description = 'RacePro tools',
+    author = 'Oren Laadan',
+    author_email = 'orenl@cs.columbia.edu',
+    package_dir = {'': 'src'},
+    scripts=['src/racepro']
+)
