c4ac749fbb26e0de2e77889f63b5cd3aae6b9683
Junfeng Yang <junfeng@rcs.(none)>
2011-03-06 14:41:50 -0500

    new testcase for loosecut

---
+++ tests/read_write_write.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define FILE  "/tmp/read-write-write.out"
+
+main()
+{
+        char buf[16];
+	int fd;
+	int ret;
+
+        memset(buf, 'X', sizeof buf);
+
+	unlink(FILE);
+	fd = open(FILE, O_RDWR | O_CREAT | O_EXCL , 0);
+	if (fd < 0) {
+		perror("open");
+		exit(1);
+	}
+        write(fd, buf, sizeof buf);
+        lseek(fd, 0, SEEK_SET);
+
+	ret = fork();
+        switch(ret) {
+        case -1:
+		perror("fork");
+		exit(1);
+        case 0:
+                memset(buf, 'C', sizeof buf);
+                write(fd, buf, sizeof buf);
+                break;
+        default:
+                read(fd, buf, sizeof buf);
+                memset(buf, 'P', sizeof buf); 
+                lseek(fd, 0, SEEK_SET);
+                write(fd, buf, sizeof buf);
+        }
+	exit(0);
+}
