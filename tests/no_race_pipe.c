d11a48861756bde91f4163cd12cee57fceb89c49
Junfeng Yang <junfeng@cs.columbia.edu>
2011-03-08 12:57:51 -0500

    another testcase: no_race_pipe.  once pipe works, no race should be reported and no replay diverge errors should show up

--- tests/no_race_pipe.c
+++ tests/no_race_pipe.c
@@ -8,9 +8,15 @@
 
 main()
 {
+        int pipefd[2];
         char buf[16];
 	int fd;
 	int ret;
+
+        if(pipe(pipefd) < 0) {
+                perror("pipe");
+                exit(1);
+        }
 
 	unlink(FILE);
 	fd = open(FILE, O_RDWR | O_CREAT | O_EXCL , 0);
@@ -25,15 +31,24 @@ main()
 		perror("fork");
 		exit(1);
         case 0:
+                close(pipefd[1]);
+                read(pipefd[0], buf, sizeof buf);
+                close(pipefd[0]);
+
                 memset(buf, 'C', sizeof buf);
-                printf("child writes\n");
+                printf("child writes after read from pipe\n");
                 write(fd, buf, sizeof buf); // no race
                 break;
         default:
                 memset(buf, 'P', sizeof buf);
-                wait(NULL);
-                printf("parent writes after child return\n");
+                printf("parent writes\n");
                 write(fd, buf, sizeof buf); // no race
+
+                close(pipefd[0]);
+                write(pipefd[1], buf, sizeof buf);
+                close(pipefd[1]);
+
+                wait(NULL);
         }
         close(fd);
 	exit(0);
