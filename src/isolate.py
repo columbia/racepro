12ec146b88b1c41f33e1922a4691ad020059dc52
Oren Laadan <orenl@cs.columbia.edu>
2011-03-15 00:36:06 -0400

    isolate: mix fixes

--- src/isolate.py
+++ src/isolate.py
@@ -8,14 +8,12 @@ class Isolate:
     def execute(self, command, chroot=True):
         if not self.mounted:
             raise OSError 
-        return subprocess.call(['sudo', 'chroot', self.mount])
         if not chroot:
             cmd = command.split()
         else:
-            pre = 'sudo chroot %s /bin/bash -c' % self.mount
+            pre = 'sudo chroot %s /bin/sh -c' % self.mount
             cmd = pre.split()
-            cmd.append('"cd %s; exec %s"' % (os.getcwd(), ' '.join(command)))
-        print('cmd = %s' % cmd)
+            cmd.append('cd %s; exec %s' % (os.getcwd(), ' '.join(command)))
         return subprocess.call(cmd)
 
     def scratch_path(self):
@@ -24,16 +22,36 @@ class Isolate:
     def mount_path(self):
         return self.mount
 
-    def __umount_dirs(self):
-        cmd = 'sudo umount %s/proc' % self.mount
+    def close(self):
+        cmd = 'sudo fusermount -u %s' % (self.mount)
+        ret = subprocess.call(cmd.split())
+        if ret != 0:
+            print('fusertmount -u %s: failed with exit %d' % (self.mount, ret))
+            return False
+        return True
+
+    def open(self):
+        cmd = \
+            'sudo unionfs-fuse' + \
+            ' -o cow,allow_other,use_ino,suid,dev,nonempty,max_files=32768' + \
+            ' %s=rw:%s=ro %s' % (self.scratch, self.root, self.mount)
+        ret = subprocess.call(cmd.split())
+        if ret != 0:
+            print('unionfs-fuse of "%s", "%s" --> "%s": failed with exit %d' %
+                  (self.root, self.scratch, self.mount, ret))
+            return False
+        return True
+
+    def _umount_dirs(self):
+        cmd = 'sudo umount -l %s/proc' % self.mount
         subprocess.call(cmd.split())
-        cmd = 'sudo umount %s/dev' % self.mount
+        cmd = 'sudo umount -l %s/dev' % self.mount
         subprocess.call(cmd.split())
         if self.persist:
-            cmd = 'umount -o bind %s/%s' % (self.mount, self.persist)
+            cmd = 'umount -l %s/%s' % (self.mount, self.persist)
             subprocess.call(cmd.split())
 
-    def __mount_dirs(self):
+    def _mount_dirs(self):
         cmd = 'sudo mount -o bind /proc %s/proc' % self.mount
         subprocess.call(cmd.split())
         cmd = 'sudo mount -o bind /dev %s/dev' % self.mount
@@ -43,35 +61,17 @@ class Isolate:
                 (self.persist, self.mount, self.persist)
             subprocess.call(cmd.split())
 
-    def __close(self):
-        cmd = 'sudo fusermount -u %s' % (self.mount)
-        ret = subprocess.call(cmd.split())
-        if ret != 0:
-            print('fusertmount -u %s: failed with exit %d' % (self.mount, ret))
-            return False
-        return True
-
-    def __open(self):
-        cmd = \
-            'sudo unionfs-fuse' + \
-            ' -o cow,allow_other,use_ino,suid,dev,nonempty,max_files=32768' + \
-            ' %s=rw:%s=ro %s' % (self.scratch, self.root, self.mount)
-        print(cmd)
-        ret = subprocess.call(cmd.split())
-        if ret != 0:
-            print('unionfs-fuse of "%s", "%s" --> "%s": failed with exit %d' %
-                  (self.root, self.scratch, self.mount, ret))
-            return False
-        return True
-
     def __exit__(self, type, value, tb):
         self.mounted = False
-        self.__umount_dirs()
-        self.__close()
+        self._umount_dirs()
+        self.close()
+        for dir in self._rmdirs:
+            cmd = 'rm -rf %s' % self.scratch
+            subprocess.call(cmd.split())
 
     def __enter__(self):
-        self.__open()
-        self.__mount_dirs()
+        self.open()
+        self._mount_dirs()
         self.mounted = True
         return self
 
@@ -81,10 +81,14 @@ class Isolate:
                  scratch=None,
                  persist=None):
 
+        self._rmdirs = list()
+
         if not scratch:
             scratch = tempfile.mkdtemp(prefix='isolate-temp-')
+            self._rmdirs.append(scratch)
         if not mount:
             mount = tempfile.mkdtemp(prefix='isolate-temp-')
+            self._rmdirs.append(mount)
 
         self.mounted = False
         self.root = root
