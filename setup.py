b4c537017936b384a8bad1ec20a9c6f5f0408e45
Nicolas Viennot <nicolas@viennot.biz>
2011-04-12 17:22:52 -0400

    Using a standard package structure

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description = 'RacePro tools',
     author = 'Oren Laadan',
     author_email = 'orenl@cs.columbia.edu',
-    package_dir={'': 'src'},
-    packages=[''],
-    scripts=['src/racepro', 'src/racetest', 'src/isolate']
+    packages=['racepro'],
+    scripts=['scripts/racepro', 'scripts/racetest', 'scripts/isolate'],
+    requires=['networkx']
 )
